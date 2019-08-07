2583bfa674dafb266a80577bc66924f045eabeb6
Justin Turner Arthur <justinarthur@gmail.com>
2019-08-07 04:53:31 -0500

    Experimental ASGI support.

---
+++ serverwamp/adapters/asgi_asyncio.py
@@ -0,0 +1,97 @@
+import asyncio
+from typing import Awaitable, Callable, Mapping
+
+from serverwamp.adapters import asgi_base, base
+from serverwamp.protocol import WAMPProtocol
+
+get_event_loop = getattr(asyncio, 'get_running_loop', asyncio.get_event_loop)
+
+
+class WSTransport(asgi_base.WSTransport):
+    def __init__(
+        self,
+        loop,
+        asgi_scope: Mapping,
+        asgi_send: Callable[[Mapping], Awaitable]
+    ) -> None:
+        super().__init__(asgi_scope, asgi_send)
+        self._loop = loop
+        self._scheduled_tasks = set()
+
+    def send_msg_soon(self, msg: str) -> None:
+        task = self._loop.create_task(self.send_msg(msg))
+        self._scheduled_tasks.add(task)
+        task.add_done_callback(self._scheduled_tasks.remove)
+
+    async def close(self):
+        await asyncio.gather(*self._scheduled_tasks)
+        await super().close()
+
+
+class WAMPApplication(base.WAMPApplication):
+    async def asgi_application(
+        self,
+        scope: Mapping,
+        receive: Callable[[], Awaitable[Mapping]],
+        send: Callable[[Mapping], Awaitable]
+    ) -> None:
+        if scope['type'] != 'websocket':
+            raise Exception(f'Connection scope "{type}" not supported by this '
+                            'ASGI adapter.')
+
+        transport = WSTransport(get_event_loop(), scope, send)
+        if self.broker:
+            wamp_protocol = WAMPProtocol(
+                transport=transport,
+                rpc_handler=self.router.handle_rpc_call,
+                subscribe_handler=self.broker.handle_subscribe,
+                unsubscribe_handler=self.broker.handle_unsubscribe,
+                **self._protocol_kwargs
+            )
+        else:
+            wamp_protocol = WAMPProtocol(
+                transport=transport,
+                transport_authenticator=None,
+                rpc_handler=self.router.handle_rpc_call,
+                **self._protocol_kwargs
+            )
+
+        await send({
+            'type': 'websocket.accept',
+            'subprotocol': self.WS_PROTOCOLS[0]
+        })
+
+        while True:
+            event = await receive()
+            event_type = event['type']
+            if event_type == 'websocket.receive':
+                msg_text = (
+                    event.get('text')
+                    or
+                    event.get('data').decode('utf-8')
+                )
+                await wamp_protocol.handle_msg(msg_text)
+            elif event_type == 'websocket.disconnect':
+                break
+
+    def legacy_asgi_application(
+            self,
+            scope: Mapping
+    ) -> Callable[
+        [
+            Callable[[], Awaitable[Mapping]],
+            Callable[[Mapping], Awaitable]
+        ],
+        Awaitable
+    ]:
+        if scope['type'] != 'websocket':
+            raise Exception(f'Connection scope "{type}" not supported by this '
+                            f'ASGI adapter.')
+
+        async def _legacy_asgi_app_awaitable(
+            receive: Callable[[], Awaitable[Mapping]],
+            send: Callable[[Mapping], Awaitable]
+        ) -> None:
+            return await self.asgi_application(scope, receive, send)
+
+        return _legacy_asgi_app_awaitable
---
+++ serverwamp/adapters/asgi_base.py
@@ -0,0 +1,47 @@
+from abc import ABC
+from http.cookies import CookieError, SimpleCookie
+from typing import Awaitable, Callable, Mapping, Optional
+
+from serverwamp.adapters import base
+
+
+class WSTransport(base.Transport, ABC):
+    def __init__(
+        self,
+        asgi_scope: Mapping,
+        asgi_send: Callable[[Mapping], Awaitable]
+    ) -> None:
+        self.closed = False
+        self._asgi_send = asgi_send
+
+        cookies_header = asgi_scope['headers'].get(b'Cookie')
+        if cookies_header:
+            try:
+                cookie = SimpleCookie(cookies_header)
+            except CookieError:
+                self._cookies = None
+            else:
+                self._cookies = {k: v.value for k, v in cookie.items()}
+        if 'client' in asgi_scope:
+            self._remote = str(tuple(asgi_scope['client']))
+        else:
+            self._remote = None
+
+    @property
+    def cookies(self) -> Optional[Mapping[str, str]]:
+        return self._cookies
+
+    @property
+    def remote(self):
+        return self._remote
+
+    async def send_msg(self, msg: str) -> None:
+        if self.closed:
+            return
+        await self._asgi_send({
+            'type': 'websocket.send',
+            'text': msg
+        })
+
+    async def close(self):
+        await self._asgi_send({'type': 'websocket.close'})
