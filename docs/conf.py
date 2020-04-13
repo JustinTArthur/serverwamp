c425f97bf478b2050d252d6d8bea49b3a1965ef4
Justin Arthur <jarthur@lifesize.com>
2020-04-13 02:13:38 -0500

    Configure master_doc for readthedocs' sphinx builder.

--- docs/conf.py
+++ docs/conf.py
@@ -23,6 +23,7 @@ author = 'Justin Arthur'
 
 
 # -- General configuration ---------------------------------------------------
+master_doc = 'index'
 
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
